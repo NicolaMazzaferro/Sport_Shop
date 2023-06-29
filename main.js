f562b970acae801af77615a77d2dcc6c34f3e49f
NicolaMazzaferro <nicolamazzaferro9@gmail.com>
2023-06-29 20:44:25 +0200

    Aggiunte Sezione Numeri - Sezione Carousel

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Navbar
+
 let navbar = document.querySelector('#navbar')
 let navCustom = document.querySelector('.navCustom')
 
@@ -15,3 +17,93 @@ window.addEventListener('scroll', () =>{
         navCustom.classList.remove('w-100')
     }
 })
+
+// Navbar end
+
+// Section Numeri
+
+let primoNumero = document.querySelector('#primoNumero')
+let secondoNumero = document.querySelector('#secondoNumero')
+
+function createInterval(total, element, timing) {
+    
+    let counter = 0
+    let interval = setInterval(() =>{
+        if (counter < total) {
+            counter++
+            element.innerHTML = counter
+        }else{
+            clearInterval(interval)
+        }
+        console.log(counter);
+    }, timing)
+}
+
+
+let check = true
+
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && check) {
+            createInterval(1500, primoNumero, 1)
+            createInterval(2500, secondoNumero, 1)
+            check = false
+            setTimeout(() => {
+                check = true
+            }, 5000)
+        }
+    });
+})
+
+observer.observe(primoNumero)
+
+// Section Numeri end
+
+// Section Carousel
+
+var swiper = new Swiper('.mySwiper', {
+    // Optional parameters  
+    loop: true,
+    effect: "fade",
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable: true,
+    },
+  });
+
+// Articoli
+
+let rowArticolo = document.querySelector('#rowArticolo')
+
+let newArticle = ['primo_articolo', 'secondo_articolo', 'terzo_articolo']
+
+
+newArticle.forEach(el => {
+    el <= el.length
+    let article = document.createElement('div')
+
+    article.classList.add('col-12', 'col-md-3')
+        
+    article.innerHTML = `
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, totam! Omnis earum enim iste, possimus fuga facilis nemo eveniet corporis, sit pariatur maxime nihil eos eaque laboriosam, quo recusandae exercitationem!</p>
+    `
+    
+    rowArticolo.appendChild(article)
+})
+
+// Articoli end
+
+// Section Carousel end
