72a711b4cb8a9ca01f020584b06cb729259ab7d1
NicolaMazzaferro <nicolamazzaferro9@gmail.com>
2023-06-30 15:57:20 +0200

    Aggiunta Section Card

--- main.js
+++ main.js
@@ -35,7 +35,6 @@ function createInterval(total, element, timing) {
         }else{
             clearInterval(interval)
         }
-        console.log(counter);
     }, timing)
 }
 
@@ -50,7 +49,7 @@ let observer = new IntersectionObserver((entries) => {
             check = false
             setTimeout(() => {
                 check = true
-            }, 5000)
+            }, 17000)
         }
     });
 })
@@ -107,3 +106,61 @@ newArticle.forEach(el => {
 // Articoli end
 
 // Section Carousel end
+
+// Section Card
+let containerCard = document.querySelector('#containerCard')
+
+let elementCard = [
+    {titolo: 'Prodotto', descrizione: `Some quick example text to build on the card title and make up the bulk of the card's content.`, immagineProdotto: './media/card1.jpeg'
+}]
+
+function newCard(title, desciption, imgProduct) {
+    elementCard.push({titolo: title, descrizione: desciption, immagineProdotto: imgProduct})
+}
+
+function newElementCard() {
+
+    elementCard.forEach(element => {
+        let card = document.createElement('div')
+        
+        card.classList.add('col-12', 'col-md-4', 'd-flex', 'justify-content-center')
+        
+        card.innerHTML = `<div class="card rounded-5 my-5">
+        <img src="${element.immagineProdotto}" class="card-img-top rounded-5" alt="card">
+        <div class="card-body">
+        <h5 class="h3">${element.titolo}</h5>
+        <p class="card-text color-primary">${element.descrizione}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>`
+
+        containerCard.appendChild(card)
+    });
+}
+
+
+
+newCard('Prodotto1','prova Prodotto 1','./media/card2.jpeg')
+newCard('Prodotto2','prova Prodotto 2','./media/card3.jpeg')
+newCard('Prodotto3','prova Prodotto 3','./media/card4.jpeg')
+newCard('Prodotto4','prova Prodotto 4','./media/card5.jpeg')
+
+newElementCard()
+
+let newProductTitle
+let newProductDescription
+let newProductImg
+addProduct.addEventListener('click', () => {
+    newProductTitle = prompt('INSERISCI NOME PRODOTTO: ')
+    newProductDescription = prompt('INSERISCI DESCRIZIONE PRODOTTO: ')
+    newProductImg = prompt('INSERISCI IMMAGINE PRODOTTO PRODOTTO: ')
+    newCard(newProductTitle, newProductDescription, newProductImg)
+    containerCard.innerHTML = ''
+    containerCard.appendChild(buttonAdd)
+    newElementCard()
+})
+
+let buttonAdd = document.querySelector('#containerButton')
+
+
+// Section Card end
